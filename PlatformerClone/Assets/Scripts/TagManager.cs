686dd1124bf7c02ecae66626f6ae00322874c2d4
ElsFouche <166535453+ElsFouche@users.noreply.github.com>
2024-04-10 14:52:29 -0700

    Added Tag Manager

---
+++ PlatformerClone/Assets/Scripts/TagManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagManager : MonoBehaviour
+{
+
+    public enum Tags
+    {
+        None,
+        Player,
+        Enemies,
+        Bullet,
+        Pickup
+    }
+
+    public enum Bullet
+    {
+        None,
+        Normal,
+        Heavy,
+        SuperMissile
+    }
+
+    public enum Enemies
+    {
+        None,
+        Normal, 
+        Strong
+    }
+
+    public Tags tagType;
+    public Bullet bulletType;
+    public Enemies enemyType;
+}
