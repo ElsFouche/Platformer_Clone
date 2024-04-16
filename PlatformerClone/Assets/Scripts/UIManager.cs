c220f81144aa072a45ff3e05e508d8dce15e529e
sbelcher333 <marguriette@icloud.com>
2024-04-16 11:50:05 -0700

    UI and stuff

--- PlatformerClone/Assets/Scripts/UIManager.cs
+++ PlatformerClone/Assets/Scripts/UIManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
+/*
+ * Symon Belcher
+ * 4/16/2024
+ * player UI
+ */
 public class UIManager : MonoBehaviour
 {
+    public TMP_Text healthText;
+
+    public GameObject player;
+
+    private PlayerHealth playerHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerHealth = player.GetComponentInChildren<PlayerHealth>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       healthText.text = "health: " + playerHealth.health;// counts lives
     }
 }
